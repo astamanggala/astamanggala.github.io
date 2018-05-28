cc76b477f21578ce5865091bc678b732ecc143c0
sendz <sendzation@gmail.com>
2018-05-28 14:31:57 +0700

    Design is almost done

---
+++ src/components/widgets/Tree.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul v-if="family">
+    <li v-for="person in family" :key="person">
+      <a href="#" v-if="person.name">
+        <p>{{person.name}}</p>
+        <p>{{person.spouse}}</p>
+      </a>
+      <tree-node
+        v-if="person.children"
+        :family="person.children"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['family'],
+  name: 'tree-node'
+}
+</script>
---
+++ src/data/Astamanggala.js
@@ -0,0 +1,105 @@
+export default [{
+  name: 'Grand grand father',
+  spouse: 'Grand grand mother',
+  photos: {
+    self: [
+      'grand_grand_father.jpg'
+    ],
+    spouse: [
+      'grand_grand_mother.jpg'
+    ]
+  },
+  contact: {
+    phone: '081111111',
+    email: 'mail@domain.com',
+    address: 'Alamat'
+  },
+  children: [{
+    name: 'Grand father',
+    spouse: 'Grand mother',
+    photos: {
+      self: [
+        'grand_father.jpg'
+      ],
+      spouse: [
+        'grand_mother.jpg'
+      ]
+    },
+    contact: {
+      phone: '081111111',
+      email: 'mail@domain.com',
+      address: 'Alamat'
+    },
+    children: [{
+      name: 'Father',
+      spouse: 'Mother',
+      photos: {
+        self: [
+          'father.jpg'
+        ],
+        spouse: [
+          'mother.jpg'
+        ]
+      },
+      contact: {
+        phone: '081111111',
+        email: 'mail@domain.com',
+        address: 'Alamat'
+      },
+      children: [{
+        name: 'Brother',
+        spouse: 'Sister in law'
+      }, {
+        name: 'Me',
+        spouse: 'Wife',
+        photos: {
+          self: [
+            'me.jpg'
+          ],
+          spouse: [
+            'wife.jpg'
+          ]
+        },
+        contact: {
+          phone: '081111111',
+          email: 'mail@domain.com',
+          address: 'Alamat'
+        },
+        children: [{
+          name: 'Son',
+          spouse: 'Daughter in law',
+          photos: {
+            self: [
+              'son.jpg'
+            ],
+            spouse: [
+              'his_wife.jpg'
+            ]
+          },
+          contact: {
+            phone: '081111111',
+            email: 'mail@domain.com',
+            address: 'Alamat'
+          },
+          children: [{
+            name: 'Grand children',
+            spouse: '',
+            photos: {
+              self: [
+                'grand_children.jpg'
+              ],
+              spouse: [
+                ''
+              ]
+            },
+            contact: {
+              phone: '081111111',
+              email: 'mail@domain.com',
+              address: 'Alamat'
+            }
+          }]
+        }]
+      }]
+    }]
+  }]
+}]
